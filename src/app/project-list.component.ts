9b668e62e2d9eb33f9c1688f25fdf85a2b1e442f
Chris Rimes <chris.rimes@gmail.com>
2016-10-23 01:16:02 +0100

    Reproduce project list as angular 2 component.

--- src/app/project-list.component.ts
+++ src/app/project-list.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { Project } from './project';
+import { ProjectService } from './project.service';
 
 @Component({
   selector: 'project-list',
   templateUrl: './project-list.component.html',
   styleUrls: []
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements OnInit {
+  projects: Project[]
+
+  constructor(private projectService: ProjectService) { }
+
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects(): void {
+    this.projectService.getProjects().then(projects => this.projects = projects);
+  }
+
+  create(): void {
+    console.log("Create project.");
+  }
+
+  linkTo(id: number): string {
+    return '/projects/' + id;
+  }
+
 }
