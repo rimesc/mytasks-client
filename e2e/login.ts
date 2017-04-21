8a043d364f06fd52a8d340796ccfd3e49b5c6963
Chris Rimes <chris.rimes@gmail.com>
2017-04-21 15:21:05 +0100

    Improve login flow in e2e tests.

--- e2e/login.ts
+++ e2e/login.ts
@@ -2,21 +2,27 @@ import { browser, element, by, ExpectedConditions } from 'protractor';
 import { EMAIL, PASSWORD } from './credentials';
 
 export function login() {
-  if (element(by.css('auth0-lock-overlay')).isPresent()) {
-    let loginForm = element(by.css('.auth0-lock-content'));
-    let emailInput = element(by.name('email'));
-    let passwordInput = element(by.name('password'));
-    let submitButton = element(by.css('.auth0-lock-submit'));
-    browser.wait(ExpectedConditions.visibilityOf(loginForm));
-    if (!passwordInput.isPresent()) {
-      // if there is no password field, assume the last login has been remembered and click the alternative link 
-      let altLink = element(by.css('.auth0-lock-alternative-link'));
-      altLink.click();
-      passwordInput = element(by.name('password'));
-      browser.wait(ExpectedConditions.visibilityOf(passwordInput));
+  let loginOverlay = element(by.css('.auth0-lock-overlay'));
+  browser.wait(loginOverlay.isPresent().then( needsLogin => {
+    if (needsLogin) {
+      doLogin();
     }
-    emailInput.sendKeys(EMAIL);
-    passwordInput.sendKeys(PASSWORD);
-    submitButton.click();
+  }));
+}
+
+function doLogin() {
+  let loginForm = element(by.css('.auth0-lock-content'));
+  let emailInput = element(by.name('email'));
+  let passwordInput = element(by.name('password'));
+  let submitButton = element(by.css('.auth0-lock-submit'));
+  browser.wait(ExpectedConditions.visibilityOf(loginForm));
+  if (!passwordInput.isPresent()) {
+    // if there is no password field, assume the last login has been remembered and click the alternative link 
+    let altLink = element(by.css('.auth0-lock-alternative-link'));
+    altLink.click();
   }
+  browser.wait(ExpectedConditions.visibilityOf(passwordInput));
+  emailInput.sendKeys(EMAIL);
+  passwordInput.sendKeys(PASSWORD);
+  submitButton.click();
 }
